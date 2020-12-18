145d919143cc1a168d39a6f2a38fe7bb2e27e790
vito <genovese.work@gmail.com>
2020-12-18 10:06:48 +0100

    implement Apartment class

--- piso.js
+++ piso.js
@@ -1,3 +1,56 @@
+class Apartment {
+    constructor(id, adress, numberOfRooms) {
+        this.id = id;
+        this.adress = adress;
+        this.numberOfRooms = numberOfRooms;
+        this.roomiesList = [];
+        this.invoices = [];
+    }
+    addRoomie(roomie) {
+        this.roomiesList.push(roomie);
+    }
+    addInvoice(invoice) {
+        this.invoices.push(invoice);
+    }
+
+    report(invoiceGiven) {
+        // 1. Extract interval:
+
+        this.roomiesList.forEach((roomie) => {
+            const interval = [];
+            let debtPerInvoice = 0;
+            if (invoiceGiven.begin >= roomie.end || invoiceGiven.end < roomie.begin) {
+                debtors;
+            } else {
+                // Extracting the first element of interval:
+
+                if (invoiceGiven.begin >= roomie.begin) {
+                    interval.push(invoiceGiven.begin);
+                } else {
+                    interval.push(roomie.begin);
+                }
+
+                // Extracting the second element of interval:
+
+                if (invoiceGiven.end <= roomie.end) {
+                    interval.push(invoiceGiven.end);
+                } else {
+                    interval.push(roomie.end);
+                }
+
+                // 2. Insert names and debt into the dictionary debtors:
+
+                const diffOnDays = (interval[1] - interval[0]) / (60 * 60 * 24 * 1000); // Getting differences between dates and after converting ms in days.
+                console.log(diffOnDays);
+                debtPerInvoice = invoiceGiven.totalFractionated * diffOnDays;
+                invoiceGiven.debtors[roomie.name] = parseFloat(
+                    debtPerInvoice.toFixed(2)
+                );
+            }
+        });
+    }
+}
+
 class Roomie {
     constructor(name, begin, end, debt = 0) {
         this.name = name;
@@ -8,138 +61,90 @@ class Roomie {
 }
 
 class Invoice {
-    constructor(total, type, begin, end) {
-        //ask for this
+    constructor(total, type, begin, end, apartment) {
+        this.apartment = apartment;
         this.total = total;
         this.type = type;
         this.begin = begin;
         this.end = end;
-        this.payed = false;
-        this.totalFractionated = this.totalPerPerson();
+
+        this.isPayed = false;
+        this.totalFractionated = this.totalPerRoom();
         this.debtors = {};
     }
-    totalPerPerson() {
-            const { begin, end, total } = this;
-            const difference = end.getTime() - begin.getTime(); //.getTime function were eliminated for the sake of a simplified test.
-            const daysOfInterval = difference / (1000 * 60 * 60 * 24); // keeping in mind the mentioned test this has to be eliminated also.
-            const totalPerDay = total / daysOfInterval;
-            return totalPerDay / 5;
-        }
-        //Report function:
-    report() {
-        const { total, type, begin, end, payed, debtors, totalFractionated } = this;
-
-        // 1. Extract interval:
-
-        roomiesList.forEach((roomie) => {
-            const interval = [];
-            let debtPerInvoice = 0;
-            if (begin >= roomie.end || end < roomie.begin) {
-                console.log("Begin was higher or end was lower than begin");
-                debtors;
-            } else {
-                // Extracting the first element of interval:
-
-                if (begin >= roomie.begin) {
-                    interval.push(begin);
-                    console.log("invoice.begin was pushed");
-                } else {
-                    interval.push(roomie.begin);
-                    console.log("roomie.begin was pushed");
-                }
-
-                // Extracting the second element of interval:
-
-                if (end <= roomie.end) {
-                    interval.push(end);
-                } else {
-                    interval.push(roomie.end);
-                }
-                console.log(interval);
-                const diffOnDays = (interval[1] - interval[0]) / (60 * 60 * 24 * 1000); // Getting differences between dates and after converting ms into days
-                debtPerInvoice = totalFractionated * diffOnDays;
-                debtors[roomie.name] = debtPerInvoice; // Insert amount in invoice dictionary.
-                roomie.debt += debtPerInvoice; // Insert amount in roomie debt.
-            }
-            // First exit of the else statemt.
-        });
+    totalPerRoom() {
+        const numberOfRooms = this.apartment.numberOfRooms;
+        const { begin, end, total } = this;
+        const difference = end - begin;
+        const daysOfInterval = difference / (1000 * 60 * 60 * 24);
+        const totalPerDay = total / daysOfInterval;
+        return totalPerDay / numberOfRooms;
     }
 }
 
 //Creation statements:
-
-const anonymous1 = new Roomie(
-    "Anonymous1",
-    new Date(2018, 0, 1),
-    new Date(2020, 11, 31, 1)
-);
-const anonymous2 = new Roomie( //Sarah
-    "Anonymous2",
-    new Date(2020, 8, 1, 1),
-    new Date(2020, 11, 31, 1)
-);
-const anonymous3 = new Roomie( //Yausi
-    "Anonymous3",
-    new Date(2020, 9, 1, 1),
-    new Date(2020, 11, 31, 1)
-);
-const anonymous4 = new Roomie( //Anna
-    "Anonymous4",
-    new Date(2018, 0, 1, 1),
-    new Date(2020, 11, 31, 1)
-);
-const anonymous5 = new Roomie( //Norma
-    "Anonymous5",
-    new Date(2020, 8, 1, 1),
-    new Date(2020, 11, 31, 1)
-);
-const anonymous6 = new Roomie( //Oscar
-    "Anonymous6",
-    new Date(2018, 0, 1, 1),
-    new Date(2020, 8, 31, 1)
+const guzmanElBueno83 = new Apartment(
+    "1",
+    "Calle Guzman el Bueno 83, bajo C",
+    5
 );
 
-const roomiesList = [
-    anonymous1,
-    anonymous2,
-    anonymous3,
-    anonymous4,
-    anonymous5,
-    anonymous6,
-];
+//Roomies
 
-const luz = new Invoice(
-    51.46,
-    "luz",
-    new Date(2020, 8, 7, 1),
-    new Date(2020, 9, 9, 1)
+guzmanElBueno83.addRoomie(
+    new Roomie("Vito", new Date(2018, 0, 1, 2), new Date())
 );
-const gas = new Invoice(
-    58.6,
-    "gas",
-    new Date(2020, 7, 20, 1),
-    new Date(2020, 9, 20, 1)
+guzmanElBueno83.addRoomie(
+    new Roomie("Sarah", new Date(2020, 8, 1, 2), new Date())
+);
+guzmanElBueno83.addRoomie(
+    new Roomie("Yausi", new Date(2020, 9, 1, 2), new Date())
+);
+guzmanElBueno83.addRoomie(
+    new Roomie("Anna", new Date(2018, 0, 1, 2), new Date())
+);
+guzmanElBueno83.addRoomie(
+    new Roomie("Norma", new Date(2020, 8, 1, 2), new Date())
+);
+guzmanElBueno83.addRoomie(
+    new Roomie("Oscar", new Date(2018, 0, 1, 2), new Date(2020, 8, 31, 2))
+);
+guzmanElBueno83.addRoomie(
+    new Roomie("Bianca", new Date(2019, 0, 1, 2), new Date(2020, 7, 31, 2))
 );
 
-const invoices = [luz, gas];
+// Invoices:
+guzmanElBueno83.addInvoice(
+    new Invoice(
+        58.6,
+        "gas",
+        new Date(2020, 7, 20, 2),
+        new Date(2020, 9, 20, 2),
+        guzmanElBueno83
+    )
+);
+
+guzmanElBueno83.addInvoice(
+    new Invoice(
+        51.46,
+        "luz",
+        new Date(2020, 8, 7, 1),
+        new Date(2020, 9, 9, 1),
+        guzmanElBueno83
+    )
+);
+
 //Tests:
+guzmanElBueno83.report(guzmanElBueno83.invoices[0]);
+console.log(guzmanElBueno83.invoices[1]);
+// console.log(guzmanElBueno83.invoices[0]);
+// guzmanElBueno83.invoices[1].report();
+// guzmanElBueno83.invoices[1].report();
+// console.log(guzmanElBueno83.invoices[1]);
+// console.log(guzmanElBueno83);
 
-// console.log(luz);
-luz.report();
-gas.report();
-console.log(luz);
-console.log(gas);
-console.log(anonymous1);
-// console.log(anonymous2);
-// console.log(anonymous3);
-// console.log(`Start date invoice: ${luz.begin}`);
-// console.log(`End date invoice: ${luz.end}`);
-// console.log(`Total amoun per day & per room: ${luz.totalPerPerson()}`);
-// console.log(`Function test: ${luz.totalPerPerson() === 20}`);
-
-//lab:
-
-// const fecha1 = new Date(2020, 10, 10, 1);
-// const fecha2 = new Date(2020, 10, 12, 1);
-// const diff = (fecha2 - fecha1) / (60 * 60 * 24 * 1000);
-// console.log(diff);
+// const date1 = new Date(2020, 7, 20, 2);
+// const date2 = new Date(2020, 9, 20, 2);
+// const dif = date2 - date1;
+// console.log(dif / (60 * 60 * 24 * 1000));
+// console.log(guzmanElBueno83.roomiesList[0]);
